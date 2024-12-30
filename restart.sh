3bbd537ae5bd535c53bcf50069292fab76b96174
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2024-12-30 21:22:23 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 stop_services() {
-    pkill -f "dashboard-linux-amd64||nginx"
+    pkill -f "dashboard-linux-amd64"
 }
 
 start_services() {
-    nohup nginx >/dev/null 2>&1 &
     nohup ./dashboard-linux-amd64 >/dev/null 2>&1 &
 }
-echo "stop dashboard and nginx..."
+echo "stop dashboard ..."
 stop_services
-echo "start dashboard and nginx..."
+echo "start dashboard ..."
 start_services
