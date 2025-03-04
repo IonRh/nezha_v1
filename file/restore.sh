557c76b51ba97d60dd6d041665c760a2783cd733
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2025-03-04 20:37:46 +0800

    Update restore.sh

--- file/restore.sh
+++ file/restore.sh
@@ -4,7 +4,7 @@ readme_content=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: app
     "https://api.github.com/repos/$GITHUB_USERNAME/$REPO_NAME/contents/README.md")
 # Check if required environment variables are set
 if [ -z "$GITHUB_USERNAME" ] || [ -z "$REPO_NAME" ] || [ -z "$GITHUB_TOKEN" ]; then
-    echo "Error: Please set GITHUB_USERNAME, REPO_NAME, and GITHUB_TOKEN environment variables"
+    echo "Error: Please set GITHUB_USERNAME, REPO_NAME, 和 GITHUB_TOKEN environment variables"
     exit 1
 fi
 
@@ -25,18 +25,57 @@ if [ -z "$readme_content" ]; then
 fi
 if [ -n "$LATEST_BACKUP" ]; then
     # Copy backup to current directory
-    cp "$LATEST_BACKUP" ../
-
+    # cp "$LATEST_BACKUP" ../
     # Remove existing data directory and config.yml
-    rm -rf ../data
     rm -f ../config.yml
-
     # Extract new backup
-    unzip -P "$ZIP_PASSWORD" "../$LATEST_BACKUP" -d ..
-
+    unzip -P "$ZIP_PASSWORD" "$LATEST_BACKUP" -d .
+    cp config.yml ../
+    cd data
+    database="sqlite.db"
+    tables=(
+    "alert_rules"
+    "crons"
+    "ddns"
+    "nats"
+    "notification_group_notifications"
+    "notification groups"
+    "notifications"
+    "nz_waf"
+    "oauth2_binds"
+    "server_group_servers"
+    "server_groups"
+    "servers"
+    "services"
+    "transfers"
+    "users"
+    )
+    output_file="all_tables_data.sql"
+    for table in "${tables[@]}"; do
+    echo "--- Table: $table ---" >> "$output_file"
+    sqlite3 "$database" ".dump \"$table\"" >> "$output_file"
+    echo "" >> "$output_file"
+    if [ $? -ne 0 ]; then
+    echo "Error dumping table $table"
+    exit 1
+    fi
+    echo "" >> "$output_file"
+    done
+    echo "Exported all specified tables data to $output_file"
+    input_file="all_tables_data.sql"
+    output_file="all_inserts.sql"
+    grep "INSERT INTO" "$input_file" > "$output_file"
+    if [ $? -eq 0 ]; then
+    echo "Successfully extracted all INSERT statements to $output_file"
+    else
+    echo "Error extracting INSERT statements."
+    fi
+    cp all_inserts.sql /app/data
+    cd /app/data
+    sqlite3 sqlite.db "DELETE FROM users;"
+    sqlite3 sqlite.db < all_inserts.sql
     # Clean up
-    rm "../$LATEST_BACKUP"
+    rm /app/data/all_inserts.sql "/app/data/$LATEST_BACKUP"
     rm -rf /app/temp_repo
-
     echo "Restore completed successfully"
 fi
